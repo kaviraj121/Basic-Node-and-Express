c236bb0d0bb9365f16cdc61c5411cfdca1af9f9d
kaviraj121 <kavir916@gmail.coml>
2021-01-23 15:46:51 +0530

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -3,14 +3,10 @@ var app = express();
 
 console.log( "Hello World");
 
-
+app.use(express.static(__dirname + "/public"));
 app.get('/',(request,response)=>{
     response.sendFile( __dirname + "/views/index.html");
 });
-
-
-app.use(express.static(__dirname + "/public");
-
 
 
 
